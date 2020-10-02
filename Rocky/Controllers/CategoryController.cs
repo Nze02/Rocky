e1c10195d5304a153bf87d43695f136433d01cf8
Nzekwe <emmanuelnzekwe24@gmail.com>
2020-10-02 12:32:43 +0100

    feat: implement get all Categories in Index action

--- Rocky/Controllers/CategoryController.cs
+++ Rocky/Controllers/CategoryController.cs
@@ -3,14 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Rocky.Data;
+using Rocky.Models;
 
 namespace Rocky.Controllers
 {
     public class CategoryController : Controller
     {
+        private ApplicationDbContext _db;
+
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+
         public IActionResult Index()
         {
-            return View();
+            IEnumerable<Category> objList = _db.Category;
+
+            return View(objList);
         }
     }
 }
