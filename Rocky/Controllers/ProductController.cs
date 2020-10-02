609608b874281ee673d44fee72c28c0e9a47b7ee
Nzekwe <emmanuelnzekwe24@gmail.com>
2020-10-02 22:05:58 +0100

    feat: implement get all products in ProductController

--- Rocky/Controllers/ProductController.cs
+++ Rocky/Controllers/ProductController.cs
@@ -3,14 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Rocky.Data;
+using Rocky.Models;
 
 namespace Rocky.Controllers
 {
     public class ProductController : Controller
     {
+        ApplicationDbContext _db;
+
+        public ProductController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
         public IActionResult Index()
         {
-            return View();
+            IEnumerable<Product> products = _db.Products;
+
+            return View(products);
         }
     }
 }
